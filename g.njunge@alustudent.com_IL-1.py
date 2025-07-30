a7f789bc6bc720731d3a78505ffcd61159b253a9
G-Njunge <g.njunge@alustudent.com>
2025-07-30 20:07:59 +0200

    grading class

--- g.njunge@alustudent.com_IL-1.py
+++ g.njunge@alustudent.com_IL-1.py
@@ -30,6 +30,7 @@ class Assignments:
         self.choice = None
         self.form_limit = 60
         self.sum_limit = 40
+        self.grade = None
     
     def InputValidation(self, value,input_type):
         value = value.strip()
@@ -121,29 +122,96 @@ class Assignments:
                     else:
                         print("Your assignment has been recorded!")
                         break
-                
-    def user_choice(self):
+    def get_grade(self):
         while True:
-            self.choice = input("Would you like to input another assignment? Choose either 1 or 2\n1.Yes\n2.No(exit)\n")
-            self.InputValidation(self.choice, "choice")
-            if self.choice == "1":
-                self.assignment_details()
+            self.grade = input(f"Enter the grade/100: ")
+
+            if not self.grade:
+                print("The field is empty, please input a valid grade")
+            elif not self.grade.isdigit():
+                print("Invalid input, your grade should be a whole number")
+            elif not 0 <= int(self.grade) <= 100 :
+                print("Invalid input, Your grade should range between 0 and 100")
             else:
+                break
+        
+
+    # def assignment_details(self):
+    #     self.assign_name = self.get_name()
+    #     self.assign_cat = self.get_category()
+    #     if self.category == "1":
+    #         self.get_form_weight()    
+    #     else:
+    #         self.get_sum_weight()
+    #     self.assign_grade = self.get_grade()
+    #     self.user_choice()
+        
+
+class grading(Assignments) :
+    def __init__ (self):
+        super().__init__()
+        self.weighted_grade= 0 
+        self.total_weighted_grade = 0
+        self.total_weight = 0
+        self.form_overall_grade = 0
+        self.sum_overall_grade = 0
+
+    def calc_weighted_grade(self):
+        self.weighted_grade = (int(self.grade)/100 * int(self.weight))
+        self.total_weighted_grade += self.weighted_grade
+        self.total_weight += int(self.weight)
+        print(f"Your weighted grade for {self.name} assignment is {self.weighted_grade} ")
+    
+    def get_form_final_grade(self):
+        if self.total_weight == 0:
+            return 0
+        self.form_overall_grade = round((self.total_weighted_grade/self.total_weight)*100,2)
+    def get_sum_final_grade(self):
+        if self.total_weight == 0:
+            return 0
+        self.sum_overall_grade = round((self.total_weighted_grade/self.total_weight)*100,2)
+    def decision(self):
+        if self.form_overall_grade >= 30 and self.sum_overall_grade >= 20:
+            print("You have passed")
+            print(f"You have a toal formative grade of {self.form_overall_grade}")
+            print(f"You have a total summative grade of {self.sum_overall_grade}")
+        else:
+            if self.form_overall_grade < 30:
+                print("You have failed because your total formative grade is below 50% of 60") 
+            else:
+                print("You have failed because your total summative grade is below 50% of 40") 
+
+            print(f"You have a toal formative grade of {self.form_overall_grade}")
+            print(f"You have a total summative grade of {self.sum_overall_grade}")
+
+def main():
+    grader = grading()
+
+    while True:
+        grader.get_name()
+        grader.get_category()
+
+        if grader.category == "1":
+            grader.get_form_weight()
+            grader.get_grade()
+            grader.calc_weighted_grade()
+            grader.get_form_final_grade()
+        else:
+            grader.get_sum_weight()
+            grader.get_grade()
+            grader.calc_weighted_grade()
+            grader.get_sum_final_grade()
+    
+        while True:
+            choice = input("Would you like to input another assignment? Choose either 1 or 2\n1.Yes\n2.No(exit)\n")
+            if choice == "1":
+                break
+            elif choice == "2":
+                grader.decision()
                 print("Thank you for using assignment checker!")
                 sys.exit()
+            else:
+                print(" Invalid input. please enter either 1 0r 2 ")
 
-
-    def assignment_details(self):
-        self.assign_name = self.get_name()
-        self.assign_cat = self.get_category()
-        if self.category == "1":
-            self.get_form_weight()    
-        else:
-            self.get_sum_weight()
-        self.grade = int(input(f"Enter the grade/100: "))
-        self.user_choice()
+main()
         
- 
-
-Assignments1=Assignments()
-Assignments1.assignment_details()
